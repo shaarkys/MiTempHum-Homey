b1464f864f209ef26f0efbcf0f2a7dcbcf18d670
shaarkys <bretislav.kubesa@gmail.com>
2025-02-01 22:10:50 +0100

    Minor internal adjustment

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 'use strict';
 
 const Homey = require('homey');
+const { Device } = require('homey');
 const { workerData } = require('worker_threads');
 
 const SERVICE_UUID = '181a';
+
+// Capture the original method for setWarning to prevent errors : Not Found: Device with ID
+const originalSetWarning = Device.prototype.setWarning;
+
+Device.prototype.setWarning = async function(message) {
+  try {
+    await originalSetWarning.call(this, message);
+  } catch (err) {
+    this.log(`Suppressed setWarning error: ${err.message}`);
+  }
+};
+
 class MyApp extends Homey.App {
 
   /**
